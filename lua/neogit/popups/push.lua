3ebd53e9a13e5520098d7a7c387547c9d0a3f338
Iain Earl <iain@itmecho.com>
2021-07-07 22:39:32 +0100

    Hacky --set-upstream

--- lua/neogit/popups/push.lua
+++ lua/neogit/popups/push.lua
@@ -3,6 +3,7 @@ local popup = require("neogit.lib.popup")
 local status = require 'neogit.status'
 local notif = require("neogit.lib.notification")
 local git = require("neogit.lib.git")
+local input = require('neogit.lib.input')
 local a = require 'plenary.async_lib'
 local await, scheduler = a.await, a.scheduler
 
@@ -15,6 +16,17 @@ local push_upstream = function (popup)
   end
 end
 
+local push_and_set_upstream = function(popup)
+    local remote = input.get_user_input("remote> ")
+    local branch = status.repo.head.branch
+    local _, code = await(git.cli.push.args("-u", remote, branch).call())
+    if code == 0 then
+        await(scheduler())
+        notif.create("Pushed to " .. remote .. " " .. branch)
+        await(status.refresh(true))
+    end
+end
+
 function M.create()
   return popup.new()
     .name("NeogitPushPopup")
@@ -24,6 +36,7 @@ function M.create()
     .switch("d", "dry-run", "Dry run")
     .action("p", "Push to pushremote", push_upstream)
     .action("u", "Push to upstream", push_upstream)
+    .action("n", "Push and set upstream", push_and_set_upstream)
     .action("e", "Push to branch")
     .build()
 end
